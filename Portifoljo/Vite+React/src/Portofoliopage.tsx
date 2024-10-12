26b6fca486207a1bf41f92052818769472907d8d
Granit <65214984+mentos1337@users.noreply.github.com>
2024-10-12 21:13:18 +0200

    Ansvar og strukturering av komponenter

---
+++ Portifoljo/Vite+React/src/Portofoliopage.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import ProjectForm from './components/ProjectForm';
+import ProjectList from './components/ProjectList';
+import {Project} from './components/Types';
+
+const Portofoliopage: React.FC = () => {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    fetchDataFromServer();
+  }, []);
+
+  const fetchDataFromServer = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/json', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      setProjects(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleProjectSubmit = async (newProject: Project) => {
+    try {
+      await fetch('http://localhost:4000/json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newProject),
+      });
+      fetchDataFromServer();
+    } catch (error) {
+      console.error('Error submitting project:', error);
+    }
+  };
+
+  return (
+    <>
+      <header>
+        <h1>New Project</h1>
+      </header>
+      <ProjectForm onSubmit={handleProjectSubmit} />
+      <section className="ViewProjectHeader">
+        <header>
+          <h2>Projects</h2>
+        </header>
+        <button className="ShowAllProjects" onClick={fetchDataFromServer}>
+          Show all projects
+        </button>
+      </section>
+      <ProjectList projects={projects} />
+      </>
+  );
+};
+
+export default Portofoliopage;
---
+++ Portifoljo/Vite+React/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+import "../styles/Footer.css"
+export default function Footer() {
+    return (
+      <footer>
+        <p>Footer</p>
+      </footer>
+    );
+  }
