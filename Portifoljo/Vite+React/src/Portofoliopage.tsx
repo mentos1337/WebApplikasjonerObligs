9fc4ea4fb6633cf78ead3dc86b5c1ea5df3c7c4f
Granit <65214984+mentos1337@users.noreply.github.com>
2024-10-17 20:53:55 +0200

    s

--- Portifoljo/Vite+React/src/Portofoliopage.tsx
+++ Portifoljo/Vite+React/src/Portofoliopage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import ProjectForm from './components/ProjectForm';
 import ProjectList from './components/ProjectList';
 import {Project} from './components/Types';
@@ -9,45 +9,49 @@ const Portofoliopage: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const initializeData = async () => {
-      console.log('Fetching data...');
-      setLoading(true);
-      try {
-        const data = await fetchProjects();
-        setProjects(data);
-      } catch (error) {
-        setError('Feilet ved henting av data');
-      } finally {
-        setLoading(false);
-        console.log('Finished fetching data');
-      }
-    };
-
-    initializeData();
-  }, []);
-
-  const handleProjectSubmit = async (newProject: Project) => {
+  const fetchData = useCallback(async () => {
+    console.log('Fetching data...');
+    setLoading(true);
     try {
-      await submitProject(newProject);
       const data = await fetchProjects();
       setProjects(data);
     } catch (error) {
+      setError('Feilet ved henting av data');
+    } finally {
+      setLoading(false);
+      console.log('Finished fetching data');
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const handleProjectSubmit = async (newProject: Project) => {
+    try {
+      setLoading(true);
+      await submitProject(newProject);
+      await fetchData();
+    } catch (error) {
       setError('Error submitting project');
+    } finally {
+      setLoading(false);
     }
   };
+  
 
   const handleDeleteProject = async (projectId: string) => {
-    setLoading(true);
     try {
+      setLoading(true);
       await deleteProject(projectId);
-      setProjects((prevProjects) => prevProjects.filter((project) => project.Id !== projectId));
+      await fetchData();
     } catch (error) {
       setError('Error deleting project');
     } finally {
       setLoading(false);
     }
   };
+  
 
   return (
     <>
